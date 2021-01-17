e67c7932e8f5ad18593e974153f08d48796ddb00
TracyJL <71200653+TracyJL@users.noreply.github.com>
2021-01-17 18:31:38 +0800

    Add files via upload

---
+++ Score-Calculator.js
@@ -0,0 +1,26 @@
+var initialScore = 0;
+var newScore = 0;
+
+function ScoreAdder(value, id） {
+    var checkdiv = document.getElementById(`${id}`);
+    var checkbox = checkdiv.querySelector('input');
+    if(checkbox.checked === false){
+        newScore = newScore - 1
+        console.log(newScore);
+    }
+    if (checkbox.checked === true){
+        newScore = newScore +1;
+        console.log(newScore);
+    }
+
+    ScoreRes();
+}
+
+function ScoreRes(){
+    var score = document.getElementById('score');
+    score.innerText = newScore;
+}
+
+function donate(){
+    var nums = prompt("Plese enter the credits you want to donate");
+}
